bfbc96bfea695f301858c9a8e86c0858d7871406
auti117 <Austin.Formagin117@gmail.com>
2017-02-22 16:16:06 -0500

    Update Spells.java
    Updated the spells class, added in variables for damage, if it has been learned and the amount of mana required

--- src/Spells.java
+++ src/Spells.java
@@ -1,6 +1,11 @@
-///////
+///////////////////
 //Spells class, this class will hold all the possible spells that can be learned in the game
-///
+//////////////
 public class Spells{
 
+  int damage;
+  boolean learned = false;
+  int manaRequired;
+  int mana;
+  
 }
