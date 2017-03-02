02d353db0e7477d779bbd5bac91ef79e1ce093a0
Austin <austin.formagin117@gmail.com>
2017-03-02 12:25:37 -0500

    Fixed randomly generating spell damage, as well as added GUI functionality

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 //Creation Date: 2017-02-22
 //Created By: Austin Formagin
 //Most of this will just be GUI stuff that will then lead into the main game or the help
 public class TitleScreen {
+    private JPanel Welcome;
+    private JButton StartGame;
 }
