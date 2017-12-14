0caace65e4834d70abb58e29bd1e8e35fd15b44d
Austin <austin.formagin117@gmail.com>
2017-12-14 14:30:26 -0500

    Added the damage character method to the character class. It contains two if statements that control if health is added or removed from the character, to be revised in DD0.1a(Separate project from this.)

--- src/Character.java
+++ src/Character.java
@@ -48,40 +48,64 @@ public class Character
     }
 
     // -------- //
-    // mutators //
+    // Mutators //
     // -------- //
-
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //Strength stat of the character, determines the carrying capacity and ability to do
+    //strength based tasks
     public void setStr (int s){
         this.str = s;
     }
 
+    //Constitution stat of character, determines their health and how "beefy" they are
     public void setCon (int c){
         this.con = c;
     }
 
+    //Intelligence stat of the character, determines the spells that they can know
     public void setIntel (int i){
         this.intel = i;
     }
 
+    //Dexterity state of the character, determines the speed at which the character can move
+    //as well as their dodge chance
     public void setDex (int d){
         this.dex = d;
     }
 
+    //Armour, on or off.
     public void setArmor (int a){
         this.armor = a;
     }
 
+    //Sets the hit points of the character
     public void setHp (int h){
         this.hp = h;
     }
-
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
     // ---------- //
-    // behavioral //
+    // Behavioral //
     // ---------- //
-
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //Equips armour to the character
     public void donArmor(int equip){
     if (equip == 1){
         setArmor(5);
     }
+    if (equip == 0)
+        setArmor(0);
     }
+
+    public void damageChar(int dmg){
+        //Damaging character
+        if (dmg > 0)//If the damage is greater than 0, it will subtract from the players health
+            this.hp = this.hp - dmg;
+        //Healing character
+        if (dmg < 0)//If the damage is less than 0, it will add the negative of the damage
+            //to the players health.
+            this.hp = this.hp + -dmg;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
 }//End of Character Class
